3714b6db50b99590daefa25af9cd00a280af266d
MuhammadIbtisam <ibtisam@elgrocer.com>
2025-05-14 00:30:44 +0100

    issue fix

--- data_access/db_connect.py
+++ data_access/db_connect.py
@@ -1,15 +1,24 @@
 import os
+from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from data_access.models import Base
 
-DATABASE_DIR = os.path.dirname(os.path.abspath(os.path.join(os.getcwd(), 'database', 'airline_reservation.db')))
-DATABASE_URL = f"sqlite:////{os.path.join(DATABASE_DIR, 'airline_reservation.db')}"
 
-if not os.path.exists(DATABASE_DIR):
-    os.makedirs(DATABASE_DIR)
+db_path = Path(__file__).resolve().parent.parent / 'database' / 'airline_reservation.db'
+DATABASE_URL = f"sqlite:///{db_path.as_posix()}"
 
-engine = create_engine(DATABASE_URL)
+# DATABASE_DIR = os.path.dirname(os.path.abspath(os.path.join(os.getcwd(), 'database', 'airline_reservation.db')))
+# DATABASE_URL = f"sqlite:////{os.path.join(DATABASE_DIR, 'airline_reservation.db')}"
+
+# if not os.path.exists(DATABASE_DIR):
+#     os.makedirs(DATABASE_DIR)
+
+db_path.parent.mkdir(parents=True, exist_ok=True)
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+# engine = create_engine(DATABASE_URL)
 
 Base.metadata.create_all(bind=engine)
 
